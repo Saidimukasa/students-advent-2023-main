edb44d6a972f79623fd62b80ef02a92bde41e7db
Your Name <you@example.com>
2023-12-07 15:38:13 +0300

    Changes made by A96449

--- accessnumber.sh
+++ accessnumber.sh
@@ -3,3 +3,5 @@ read -p "Enter your Reg-No: " regnum
 read -p "Enter your Access Number: " accessnum
 read -p "Enter your Password: " password
 read -p "Enter your Email: " email
+read -p "Enter your Phone Number: " phone
+read -p "Enter your Name: " name
