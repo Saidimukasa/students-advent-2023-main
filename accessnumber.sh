1af49e9a450c06c29796be43bd80361c2abaef8f
Your Name <you@example.com>
2023-12-07 15:36:18 +0300

    Updates

--- accessnumber.sh
+++ accessnumber.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/env bash
 read -p "Enter your Reg-No: " regnum
+read -p "Enter your Access Number: " accessnum
+read -p "Enter your Password: " password
+read -p "Enter your Email: " email
